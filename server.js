d98dbad5d6bca6f731f49765603bd089f9fda6d7
Luisa Bider <luisabider@Luisas-MacBook-Pro.local>
2018-10-30 11:48:03 +0100

    alis changes

--- server.js
+++ server.js
@@ -1,50 +1,47 @@
-const express = require('express')
+const express = require('express');
 const bodyParser= require('body-parser')
 const app = express()
-const MongoClient = require('mongodb').MongoClient
+const { MongoClient, ObjectID }  = require('mongodb')
 
-var db
+let db
+
+function initialise() {
+  app.set('view engine', 'ejs');
+  app.use(bodyParser.urlencoded({extended: true}));
+  app.use(bodyParser.json());
+  app.use(express.static('public'));
+}
+
+function renderLink(res) {
+  db.collection('links').find({isUsed : false}).toArray((err, result) => {
+    if (err) return console.log(err)
+    // renders index.ejs
+    res.render('index.ejs', {link: result[0]});
+  });
+}
 
 MongoClient.connect('mongodb://luptilu:luisa1@ds143293.mlab.com:43293/audiodatenbank', (err, client) => {
-    if (err) return console.log(err)
-    db = client.db('audiodatenbank') // whatever your database name is
-    app.listen(3000, () => {
-      console.log('listening on 3000')
-    })
-  })
+  if (err) return console.log(err)
+  db = client.db('audiodatenbank') // whatever your database name is
+  app.listen(3000, () => {
+    console.log('listening on 3000')
+  });
+});
 
-  app.set('view engine', 'ejs')
-  app.use(bodyParser.urlencoded({extended: true}))
-  app.use(bodyParser.json())
-  app.use(express.static('public'))
+initialise();
 
-  app.get('/', (req, res) => {
-    db.collection('links').find({isUsed : false}).toArray((err, result) => {
-      if (err) return console.log(err)
-      // renders index.ejs
-      res.render('index.ejs', {links: result})
-    })
-  })
-   
+app.get('/', (req, res) => { renderLink(res); });
+
 app.post('/links', (req, res) => {
-    req.body.isUsed=false
-    db.collection('links').save(req.body, (err, result) => {
-      if (err) return console.log(err)
-  
-      console.log('In die Datenbank gespeichert. Yeah.')
-      res.redirect('/')
-    })
-    // db.collection('links').update( { _id: },
-    //   { $set:
-    //     { isUsed: true,
-    //        feedback:
-    //   )¶
-
-  })
-
-  app.set('view engine', 'ejs')
-
-  
-
-  
-  
+  let data = req.body;
+  db.collection('links').update(
+    { _id: ObjectID(data._id) },
+    { 
+      $set: { 
+        isUsed: true,
+        feedback: data.feedback,
+      }
+    }, () => {
+      renderLink(res);
+    });
+});
